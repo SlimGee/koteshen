1bba2e55ec4588640c35b8ab9fb9a69bb788ad0b
Given Ncube <givenyslim12@gmail.com>
2024-06-02 14:54:07 +0200

    feat: added the ability to recieve payments via pesepay

--- app/Http/Controllers/Billing/Pesepay/PaymentController.php
+++ app/Http/Controllers/Billing/Pesepay/PaymentController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers\Billing\Pesepay;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Pesepay\PaymentResultRequest;
 use App\Models\Invoice;
-use App\Models\User;
 use App\Pesepay\Facade\Pesepay;
+use App\Processes\Billing\Pesepay\SubscriptionPaymentProcess;
+use App\Transport\Pesepay\PaymentTransport;
 use Flixtechs\Subby\Models\Plan;
-use Illuminate\Support\Facades\Auth;
 
 class PaymentController extends Controller
 {
@@ -18,7 +18,7 @@ class PaymentController extends Controller
             'amount' => auth()->user()->hasRole('super admin') ? 0.1 : $invoice->balance,
             'currency' => 'USD',
             'reason' => 'Koteshen ' . $plan->name . ' subscription',
-            'result_url' => route('app.billing.payments.callback'),
+            'result_url' => 'https://5e71-41-174-95-140.ngrok-free.app' . route('app.billing.payments.callback', absolute: false),
             'return_url' => route('app.billing.edit'),
             'metadata' => [
                 'invoice_id' => $invoice->id,
@@ -30,32 +30,10 @@ class PaymentController extends Controller
         return Pesepay::getPaymentUrl($data)->redirectNow();
     }
 
-    public function callback(PaymentResultRequest $request)
+    public function callback(PaymentResultRequest $request, SubscriptionPaymentProcess $process)
     {
-        $details = Pesepay::verifyTransaction($request->validated('referenceNumber'));
+        $transport = PaymentTransport::make()->setRequest($request);
 
-        if ($details == false || $details['transactionStatus'] !== 'SUCCESS') {
-            return;
-        }
-
-        $user = User::find($details['metadata']['user_id']);
-        $invoice = Invoice::find($details['metadata']['invoice_id']);
-
-        Auth::login($user);
-
-        $invoice->payments()->create([
-            'amount' => $details['amountDetails']['totalTransactionAmount'],
-            'paid_at' => now(),
-            'channel' => 'pesepay',
-            'reference' => null,
-            'currency' => $details['amountDetails']['currencyCode'],
-            'business_id' => $invoice->business_id,
-            'user_id' => auth()->id(),
-            'client_id' => $invoice->client_id,
-        ]);
-
-        $plan = Plan::find($details['metadata']['plan_id']);
-
-        $user->subscribe($plan);
+        $process->run($transport);
     }
 }
