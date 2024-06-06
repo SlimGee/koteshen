73a7c1e5e7f8f37a5374e2daea30ac0e7e02f9a0
Given Ncube <givenyslim12@gmail.com>
2024-06-06 04:46:41 +0200

    fix: headline title

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use App\Enum\PostStatus;
 use Illuminate\Contracts\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
@@ -34,19 +36,32 @@ class Post extends Model
         ];
     }
 
+    public function title(): Attribute
+    {
+        return Attribute::make(get: fn($value) => Str::title($value));
+    }
+
     /**
      * Get the user that owns the post.
+     *
+     * @return BelongsTo<User>
      */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Scope a query to only include published posts.
+     */
     public function scopePublished(Builder $query): Builder
     {
         return $query->where('status', PostStatus::PUBLISHED->value);
     }
 
+    /**
+     * Get the options for generating the slug.
+     */
     public function getSlugOptions(): SlugOptions
     {
         return SlugOptions::create()
@@ -54,6 +69,9 @@ class Post extends Model
             ->saveSlugsTo('slug');
     }
 
+    /**
+     * Get the route key for the model.
+     */
     public function getRouteKeyName()
     {
         return 'slug';
